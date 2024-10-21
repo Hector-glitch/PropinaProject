94219800693b377b48181bc4bf13e66cd7865667
hector <hector.vinas@institutvidreres.com>
2024-10-21 14:17:02 +0200

    Quarto Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,10 @@ android {
         enable = true
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.hector.propinaproject"
         minSdk = 27
--- app/src/main/java/com/hector/propinaproject/MainActivity.kt
+++ app/src/main/java/com/hector/propinaproject/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.hector.propinaproject
 
 import android.annotation.SuppressLint
+import android.icu.text.NumberFormat
 import android.os.Bundle
 import android.os.TestLooperManager
 import android.widget.Button
@@ -15,20 +16,41 @@ import androidx.databinding.DataBindingUtil
 import com.hector.propinaproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     @SuppressLint("UseSwitchCompatOrMaterialCode", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.calculButton.setOnClickListener { calculateTip() }
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+            insets}
+    }
 
+    private fun calculateTip() {
+        val stringInTextField = binding.CostService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.tipAmazing -> 0.20
+            R.id.tipGood -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        if (binding.RoundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
-
-//Cost = findviewById(R.id.baba)
-//cost.text.toString()
