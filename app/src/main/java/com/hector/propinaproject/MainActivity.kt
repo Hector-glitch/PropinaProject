bc1127efa863763eeac97b102535f9cd522cdb65
hector <hector.vinas@institutvidreres.com>
2024-10-24 11:11:07 +0200

    Quinto Commit

--- app/src/main/java/com/hector/propinaproject/MainActivity.kt
+++ app/src/main/java/com/hector/propinaproject/MainActivity.kt
@@ -1,28 +1,21 @@
 package com.hector.propinaproject
 
-import android.annotation.SuppressLint
 import android.icu.text.NumberFormat
 import android.os.Bundle
-import android.os.TestLooperManager
-import android.widget.Button
-import android.widget.RadioGroup
-import android.widget.Switch
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.databinding.DataBindingUtil
 import com.hector.propinaproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    @SuppressLint("UseSwitchCompatOrMaterialCode", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.calculButton.setOnClickListener { calculateTip() }
